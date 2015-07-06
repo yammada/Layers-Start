e3d62300716a7db1a5eede77505e1ea593afa4a2
jungjihun <browniz421@gmail.com>
2015-07-07 00:14:24 +0900

    remove old old code

--- Layers/RainforestCardCell.swift
+++ Layers/RainforestCardCell.swift
@@ -9,20 +9,10 @@
 import UIKit
 
 class RainforestCardCell: UICollectionViewCell {
-  let featureImageView = UIImageView()
-  let backgroundImageView = UIImageView()
-  let titleLabel = UILabel()
-  let descriptionTextView = UITextView()
-  let gradientView = GradientView()
   var featureImageSizeOptional: CGSize?
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    contentView.addSubview(backgroundImageView)
-    contentView.addSubview(featureImageView)
-    contentView.addSubview(gradientView)
-    contentView.addSubview(titleLabel)
-    contentView.addSubview(descriptionTextView)
     contentView.layer.borderColor = UIColor(hue: 0, saturation: 0, brightness: 0.85, alpha: 0.2).CGColor
     contentView.layer.borderWidth = 1
   }
@@ -38,17 +28,6 @@ class RainforestCardCell: UICollectionViewCell {
   
   override func layoutSubviews() {
     super.layoutSubviews()
-    
-    let featureImageSize = featureImageSizeOptional ?? CGSizeZero
-    
-    backgroundImageView.frame = FrameCalculator.frameForBackgroundImage(containerBounds: bounds)
-    featureImageView.frame = FrameCalculator.frameForFeatureImage(featureImageSize: featureImageSize,
-      containerFrameWidth: frame.size.width)
-    titleLabel.frame = FrameCalculator.frameForTitleText(containerBounds: bounds,
-      featureImageFrame: featureImageView.frame)
-    descriptionTextView.frame = FrameCalculator.frameForDescriptionText(containerBounds: bounds,
-      featureImageFrame: featureImageView.frame)
-    gradientView.frame = FrameCalculator.frameForGradient(featureImageFrame: featureImageView.frame)
   }
   
   //MARK: Cell Reuse
@@ -61,24 +40,6 @@ class RainforestCardCell: UICollectionViewCell {
     //MARK: Image Size Section
     let image = UIImage(named: cardInfo.imageName)!
     featureImageSizeOptional = image.size
-    
-    // Remove everything below this comment:
-    backgroundImageView.contentMode = .ScaleAspectFill
-    backgroundImageView.image = image.applyBlurWithRadius(30, tintColor: UIColor(white: 0.5, alpha: 0.3),
-      saturationDeltaFactor: 1.8, maskImage: nil)
-    
-    featureImageView.contentMode = .ScaleAspectFit
-    featureImageView.image = image
-    
-    titleLabel.backgroundColor = UIColor.clearColor()
-    titleLabel.attributedText = NSAttributedString.attributedStringForTitleText(cardInfo.name)
-    
-    descriptionTextView.backgroundColor = UIColor.clearColor()
-    descriptionTextView.editable = false
-    descriptionTextView.scrollEnabled = false
-    descriptionTextView.attributedText = NSAttributedString.attributedStringForDescriptionText(cardInfo.description)
-    
-    gradientView.setNeedsDisplay()
   }
   
 }
